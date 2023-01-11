d7789f6fbbab0c6e6d7fa7f2071c8c1355a6d807
DavidK875 <david.kirsch@educanet.cz>
2023-01-11 10:03:24 +0100

    Create KontrolaFormulare.php

---
+++ Hodina 2/KontrolaFormulare.php
@@ -0,0 +1,21 @@
+<?php
+
+
+echo 
+"<H1>Ukázka formuláře s jednoduchou kontrolou</h1>
+<form method=POST>
+<input type='text' placeholder='Jmeno a Prijmeni' name = 'jmeno'>
+<input type='text' placeholder='adresa' name = 'adresa'>
+<input type='submit' value='odeslat' name='odeslat'>
+</form>";
+if(isset($_POST["odeslat"])){
+	$jmeno = $_POST["jmeno"];
+	$exploze =count(explode(" ",$jmeno));
+	$adresa = $_POST["adresa"];
+	if (strlen($adresa)<10){
+		echo "<p style='color:tomato'>Adresa musi obshovat alespon 10 znaku</p>";
+	}
+	if ($exploze<2){
+		echo "<p style='color:tomato'>Zadejte jmeno i prijmeni</p>";
+	}
+}
